584bce5e7cad8220e83ab19bfa229eeeb461bd0e
Bhavani Shankar <ebhavanishankar@gmail.com>
2020-12-05 19:02:34 +0530

    Simplify day 3 solution

--- src/day3.rs
+++ src/day3.rs
@@ -1,37 +1,26 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day3)]
-fn parse_input_day3(input: &str) -> Result<Vec<Vec<bool>>, String> {
-    input
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|c| match c {
-                    '#' => Ok(true),
-                    '.' => Ok(false),
-                    _ => Err("Invalid char".to_owned()),
-                })
-                .collect()
-        })
-        .collect()
+fn parse_input_day3(input: &str) -> Result<Vec<Vec<char>>, String> {
+    input.lines().map(|l| Ok(l.chars().collect())).collect()
 }
 
-fn walk_trees(trees: &[Vec<bool>], x_inc: usize, y_inc: usize) -> usize {
+fn walk_trees(trees: &[Vec<char>], x_inc: usize, y_inc: usize) -> usize {
     (0..trees.len())
         .step_by(y_inc)
         .enumerate()
         .map(|(i, y)| ((i * x_inc) % trees[y].len(), y))
-        .filter(|&(x, y)| trees[y][x])
+        .filter(|&(x, y)| trees[y][x] == '#')
         .count()
 }
 
 #[aoc(day3, part1)]
-fn part1(trees: &[Vec<bool>]) -> usize {
+fn part1(trees: &[Vec<char>]) -> usize {
     walk_trees(trees, 3, 1)
 }
 
 #[aoc(day3, part2)]
-fn part2(trees: &[Vec<bool>]) -> usize {
+fn part2(trees: &[Vec<char>]) -> usize {
     [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
         .iter()
         .map(|&(x, y)| walk_trees(trees, x, y))
