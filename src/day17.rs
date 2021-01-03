c65a81d042252eaae966bc0e11cb4fa29cc4e516
Bhavani Shankar <ebhavanishankar@gmail.com>
2021-01-04 03:52:20 +0530

    Add part 2 for day 17

--- src/day17.rs
+++ src/day17.rs
@@ -1,10 +1,8 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use fnv::FnvHashSet;
 
-type Point = (i8, i8, i8);
-
 #[aoc_generator(day17)]
-fn parse_input_day17(input: &str) -> Result<FnvHashSet<Point>, String> {
+fn parse_input_day17(input: &str) -> Result<FnvHashSet<Vec<i8>>, String> {
     input
         .lines()
         .enumerate()
@@ -12,42 +10,58 @@ fn parse_input_day17(input: &str) -> Result<FnvHashSet<Point>, String> {
             l.chars()
                 .enumerate()
                 .filter(|(_, c)| *c == '#')
-                .map(move |(j, _)| Ok((i as i8, j as i8, 0)))
+                .map(move |(j, _)| Ok(vec![i as i8, j as i8]))
         })
         .collect()
 }
 
-fn base3(n: i8) -> Point {
-    (n % 3, (n / 3) % 3, (n / 9) % 3)
+fn base3(n: i8, dim: i8) -> Vec<i8> {
+    let base: i8 = 3;
+    (0..dim).map(|d| (n / base.pow(d as u32)) % base).collect()
 }
 
-fn get_neighbors(cube: &Point) -> FnvHashSet<Point> {
-    (1..=27)
-        .map(|i| {
-            let (x, y, z) = base3(i);
-            (cube.0 + x - 1, cube.1 + y - 1, cube.2 + z - 1)
-        })
-        .filter(|e| e != cube)
+fn origin_neighbors(dim: i8) -> FnvHashSet<Vec<i8>> {
+    let base: i8 = 3;
+    (1..=base.pow(dim as u32))
+        .map(|i| base3(i, dim).iter().map(|c| c - 1).collect())
+        .filter(|e: &Vec<_>| e.iter().any(|&v| v != 0))
         .collect()
 }
 
-fn active_neighbors(cube: &Point, active: &FnvHashSet<Point>) -> usize {
-    get_neighbors(cube).intersection(active).count()
+fn get_neighbors(cube: &[i8], origin_neighbors: &FnvHashSet<Vec<i8>>) -> FnvHashSet<Vec<i8>> {
+    origin_neighbors
+        .iter()
+        .map(|n| n.iter().enumerate().map(|(i, c)| c + cube[i]).collect())
+        .collect()
 }
 
-fn cycle(active: &FnvHashSet<Point>) -> FnvHashSet<Point> {
+fn active_neighbors(
+    cube: &[i8],
+    active: &FnvHashSet<Vec<i8>>,
+    origin_neighbors: &FnvHashSet<Vec<i8>>,
+) -> usize {
+    get_neighbors(cube, origin_neighbors)
+        .intersection(active)
+        .count()
+}
+
+fn cycle(
+    active: &FnvHashSet<Vec<i8>>,
+    origin_neighbors: &FnvHashSet<Vec<i8>>,
+) -> FnvHashSet<Vec<i8>> {
     let mut to_remove = FnvHashSet::default();
     let mut to_add = FnvHashSet::default();
 
     for cube in active.iter() {
-        if ![2, 3].contains(&active_neighbors(cube, active)) {
+        if ![2, 3].contains(&active_neighbors(cube, active, origin_neighbors)) {
             to_remove.insert(cube);
         }
 
         to_add.extend(
-            get_neighbors(cube)
+            get_neighbors(cube, origin_neighbors)
                 .difference(active)
-                .filter(|c| active_neighbors(c, active) == 3),
+                .filter(|c| active_neighbors(c, active, origin_neighbors) == 3)
+                .cloned(),
         )
     }
 
@@ -55,23 +69,22 @@ fn cycle(active: &FnvHashSet<Point>) -> FnvHashSet<Point> {
         .iter()
         .chain(&to_add)
         .filter(|e| !to_remove.contains(e))
-        .copied()
+        .cloned()
         .collect()
 }
 
 #[aoc(day17, part1)]
-fn part1(active: &FnvHashSet<Point>) -> usize {
-    let mut a = cycle(active);
-    (0..5).for_each(|_| a = cycle(&a));
+fn part1(active: &FnvHashSet<Vec<i8>>) -> usize {
+    let mut a = active.iter().map(|e| vec![e[0], e[1], 0]).collect();
+    let on = origin_neighbors(3);
+    (0..6).for_each(|_| a = cycle(&a, &on));
     a.len()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn neighbors_are_26() {
-        assert_eq!(get_neighbors(&(0, 0, 0)).len(), 26);
-    }
+#[aoc(day17, part2)]
+fn part2(active: &FnvHashSet<Vec<i8>>) -> usize {
+    let mut a = active.iter().map(|e| vec![e[0], e[1], 0, 0]).collect();
+    let on = origin_neighbors(4);
+    (0..6).for_each(|_| a = cycle(&a, &on));
+    a.len()
 }
